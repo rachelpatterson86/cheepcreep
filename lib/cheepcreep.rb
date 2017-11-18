eaefd92d4fa7df7f71e30d12206db74483904203
Rachel Patterson <rachel.patterson.1986@gmail.com>
2017-11-18 12:53:17 -0500

    Update getting follower and user information from Github. Completed basic premise of original assignment.

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -4,21 +4,26 @@ require "httparty"
 require "pry"
 require 'json'
 
-#response = HTTParty.get("https://api.github.com/users/redline6561/gists")
-#browser = https://api.github.com/users/redline6561/gists
-# in terminal => ENV['apitest'] ENV ['IronYard1!'] bundle exec ruby lib/cheepcreep.rb
-# TODO: QUESTION = un/pw, are those dummie?
-# TODO: QUESTION = review option.merge!
-# TODO: QUESTION = review query param syntax. gh docs
-# TODO: QUESTION = review query param logic
-# TODO: QUESTION = look at and review : #why before JSON
-# TODO: QUESTION = review update_user fx
-# TODO: QUESTION = attr_* review
-# TODO: QUESTION = keywords and yield
-
+# in terminal => `GITHUB_USER=foo GITHUB_PASS=bar bundle exec ruby lib/cheepcreep.rb ARGV`
 module Cheepcreep
   class GithubUser < ActiveRecord::Base
     validates :login, uniqueness: true, presence: true
+    scope :order_desc, -> { order("#{column_name}": :desc) }
+
+    def self.add(response)
+      create('login'        => response['login'],
+             'name'         => response['name'],
+             'blog'         => response['blog'],
+             'public_repos' => response['public_repos'],
+             'followers'    => response['followers'],
+             'following'    => response['following']
+            )
+    end
+
+    private
+    def self.column_name
+      column_names.include?(ARGV[0]) ? ARGV[0] : 'followers'
+    end
   end
 end
 
@@ -32,94 +37,71 @@ class Github
     @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
   end
 
-  def get_followers(username, per_page = 50, page = 2)
-    options= {:query => {:per_page => per_page, :page => page}}
-    options.merge!({:basic_auth => @auth})
-    follower_list = []
-    response = self.class.get("/users/#{username}/followers", options) #can also use Github.get...
-    puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
-    binding.pry
-    json = JSON.parse(response.body)
-    json.sample(20).each do |f|
-      followers_list << f["login"]
-    end
-    followers_list
+  def run(username)
+    follower_usernames = followers(username).sample(20)
+    add_followers(follower_usernames)
+    Cheepcreep::GithubUser.order_desc.each { |user| puts user.login }
   end
 
-  def get_users_all( per_page = 50, page = 2)
-    options= {:query => {:per_page => per_page, :page => page}}
-    response = self.class.get("/users")
-    puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
-    json = JSON.parse(response.body)
+  def followers(username)
+    response = self.class.get("/users/#{username}/followers")
+    puts "#{response.headers['x-ratelimit-remaining']} request left"
+
+    followers = JSON.parse(response.body)
+    followers.inject([]) { |memo, follower| memo << follower['login']}
   end
 
-  def get_users(username)
-    response = self.class.get("/users")
-    puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
-    json = JSON.parse(response.body)
+  def add_followers(usernames)
+    usernames.each { |username| Cheepcreep::GithubUser.add(user(username)) }
   end
 
-  def update_user (options={})#nil no work...do't use query params unless get req, need to pass some body.
-    options = {:body =>{:name => name, :email => email, :blog => blog, :location => location}.json}
-    self.class.patch('/user',options) #need options
+  def user(username)
+    response = self.class.get("/users/#{username}")
+    puts "#{response.headers['x-ratelimit-remaining']} request left"
+
+    JSON.parse(response.body)
   end
 
-  def get_team_members(id)
-    response = self.class.get("/teams/#{id}/members")
-  end
+  # def update_user (options={})#nil no work...do't use query params unless get req, need to pass some body.
+  #   options = {:body =>{:name => name, :email => email, :blog => blog, :location => location}.json}
+  #   self.class.patch('/user',options) #need options
+  # end
+
+  # def get_team_members(id)
+  #   response = self.class.get("/teams/#{id}/members")
+  # end
 
 #list Gists
-  def get_gists(username)
-    response = self.class.get("/users/#{username}/gists")
-    puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
-    gists_json = JSON.parse(response.body)
-    get_gists_id(gists_json)
-  end
-
-  def get_gists_id(gists_json)
-    gists_id = []
-    gists_json.each do |id|
-      id["id"] << gist_id
-    end
-    gist_id
-  end
-
-#delete Gists
-  def delete_gists(id)
-    response = self.class.delete("/gists/#{id}")
-    puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
-  end
-
-  
-
-
-
-  def create_repo(opts={})
-    options = {:body => opts.to_json}
-  response = self.class.post("/users/repos", options)
-  JSON.parse(response.body)
-  end
+#   def get_gists(username)
+#     response = self.class.get("/users/#{username}/gists")
+#     puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
+#     gists_json = JSON.parse(response.body)
+#     get_gists_id(gists_json)
+#   end
+#
+#   def get_gists_id(gists_json)
+#     gists_id = []
+#     gists_json.each do |id|
+#       id["id"] << gist_id
+#     end
+#     gist_id
+#   end
+#
+# #delete Gists
+#   def delete_gists(id)
+#     response = self.class.delete("/gists/#{id}")
+#     puts "#{response.headers['x-ratelimit-remaining']} request left" #why before JSON
+#   end
+#
+#   def create_repo(opts={})
+#     options = {:body => opts.to_json}
+#     response = self.class.post("/users/repos", options)
+#     JSON.parse(response.body)
+#   end
 end
 
 class CheepcreepApp
 end
-
-
-def add_user(json)
-user = Cheepcreep::GithubUser.new
-[:login, :name, :blog, :public_repos, :followers, :following].each do |k|
-  user[k] = json[k.to_s]
-  #active rec model can index like a hash for the table
-  end
-  user.save
-  #or put all credentials in a hash and then insert hash into db...
-end
-
 binding.pry
-creeper = CheepcreepApp.new("redline6561")
-Cheepcreep::GithubUser.find_or_create_by('login'        => response['login'],
-                                         'blog'         => response['blog'],
-                                         'public_repos' => response['public_repos'],
-                                         'public_repos' => response['followers'],
-                                         'following'    => response['following']
-                                         )
+
+# Github.new.run('rachelpatterson86')
